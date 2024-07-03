5f4c5dbb87cff9ac16c20b273d0db42042285d8d
dan0154 <daniel_schmitz3@estudante.sesisenai.org.br>
2024-07-03 12:11:41 -0300

    Tratamento de erros

--- src/pages/AdressInput/AdressInput.jsx
+++ src/pages/AdressInput/AdressInput.jsx
@@ -4,39 +4,31 @@ import "./AdressInput.css"
 import { useEffect } from "react"
 
 export default function AdressInput(){
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
     const [searchCEP, setSearchCEP] = useState('')
     const [apiResponse, setApiResponse] = useState('')
+    const [err, setErr] = useState(false)
     
-    useEffect(() => {
-        fetch(`https://viacep.com.br/ws/${searchCEP}/json/`)
-            .then(response => response.json())
-            .then(data => {
-                setApiResponse(data)
-            })
-
-            .catch(err => {
-                console.log(err)
-                setError(err)
-            })
-
-            .finally(() => {
-                setLoading(false)
-            })
-    }, [searchCEP])
-
-    const handleInput = () => {
-        setSearchCEP(document.getElementById("CEP").value)
+    const fetchData = async () => {
+        setLoading(true)
+        try{
+            const response = await fetch(`https://viacep.com.br/ws/${searchCEP}/json/`)
+            const data = await response.json()
+            setApiResponse(data)
+            setErr(false)
+        } catch (error){
+            console.error("Houve um erro")
+            setErr(true)
+        }
+        setLoading(false)
     }
 
     return(
         <div className="centerInput">
-            <input type="text" name="CEP" id="CEP"/>
-            <button onClick={handleInput}>Buscar</button>
-            {searchCEP !== '' && <AdressDisplay item={apiResponse}/>}
-            {searchCEP === '' && loading != true && <p>Digite o CEP</p>}
-            {loading && <p>Carregando...</p>}
+            <input value={searchCEP} type="text" name="CEP" id="CEP" onChange={(e) => setSearchCEP(e.target.value)}/>
+            <button onClick={fetchData}>Buscar</button>
+            {apiResponse !== '' && err != true && !loading  && <AdressDisplay item={apiResponse}/> || loading && <p>Carregando...</p> || err && <p>Houve um erro. Digite um CEP válido ou tente novamente mais tarde.</p>}
+            {apiResponse === '' && loading != true && !err && <p>Digite o CEP</p>}
         </div>
 
     )
